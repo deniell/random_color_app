1aaec720af66112deeb3cea1e35207fe3f203fc3
Denis Stahovsky <denis.stahovsky@vantino.ch>
2023-08-30 21:50:46 +0300

    - Colored page updated with backgroundColor variable and GestureDetector to listen for tap events; - centered text adopted to screen width.

--- lib/pages/colored_page.dart
+++ lib/pages/colored_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ///
 /// Main app page, with background color randomly generated
@@ -11,13 +12,33 @@ class ColoredPage extends StatefulWidget {
 }
 
 class _ColoredPageState extends State<ColoredPage> {
+  // page background color
+  Color? backgroundColor = Colors.grey[300];
+
   @override
   Widget build(BuildContext context) {
+    // get screen width
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Hello there',
+      body: GestureDetector(
+        child: Container(
+          color: backgroundColor,
+          child: Center(
+            child: Text(
+              'Hello there',
+              style: GoogleFonts.aBeeZee(
+                // adopt font size to screen width
+                fontSize: 25 * screenWidth/200,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+          ),
         ),
+        onTap: () {
+          print('Color have to be changed');
+        },
       ),
     );
   }
