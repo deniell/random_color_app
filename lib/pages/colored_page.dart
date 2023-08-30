f02f36369e201fd177fa10a3696c6d905d400bc8
Denis Stahovsky <denis.stahovsky@vantino.ch>
2023-08-30 20:27:14 +0300

    - removed standard basic app code; - added Colored page, which will be the only app page, and so home page.

---
+++ lib/pages/colored_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ColoredPage extends StatefulWidget {
+  const ColoredPage({Key? key}) : super(key: key);
+
+  @override
+  _ColoredPageState createState() => _ColoredPageState();
+}
+
+class _ColoredPageState extends State<ColoredPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello there',
+          style: ,
+        ),
+      ),
+    );
+  }
+}
