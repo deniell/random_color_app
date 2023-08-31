cd5eae9bdde501cdcf1dc8f83db685c0ecc55da7
Denis Stahovsky <denis.stahovsky@vantino.ch>
2023-08-31 18:43:53 +0300

    - first app version is ready.

--- lib/pages/colored_page.dart
+++ lib/pages/colored_page.dart
@@ -17,7 +17,6 @@ class _ColoredPageState extends State<ColoredPage> {
   // page background color
   Color backgroundColor = Colors.grey[300] ?? Colors.grey;
   // previous page background color
-  // Color previousColor = Colors.green;
   Color previousColor = Colors.grey;
   // scale factor for text animation
   double scaleFactor = 1.0;
@@ -31,74 +30,87 @@ class _ColoredPageState extends State<ColoredPage> {
     // get status bar height
     final statusBarHeight = mediaQuery.viewPadding.top;
 
+    // animated text for center of the page
+    final Widget animatedText = AnimatedContainer(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+      transform: Matrix4.identity()..scale(scaleFactor),
+      transformAlignment: Alignment.center,
+      child: Text(
+        'Hello there!',
+        style: GoogleFonts.aBeeZee(
+          // adopt font size to screen width
+          fontSize: 25 * screenWidth/200,
+          fontWeight: FontWeight.bold,
+          color: previousColor,
+          shadows: [
+            Shadow(
+              blurRadius: 12,
+              color: previousColor,
+              offset: isMobile()
+                  ? const Offset(3, 3)
+                  : const Offset(10, 10),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    // animated colored container
+    final Widget animatedContainer = GestureDetector(
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        color: backgroundColor,
+        child: Center(
+          child: animatedText,
+        ),
+      ),
+      onTap: () {
+        // start text scale up
+        animateText();
+        // get random color and update background color with it
+        setState(() {
+          previousColor = backgroundColor;
+          backgroundColor = getRandomColor();
+        });
+      },
+    );
+
+    // close button for mobile devices
+    final Widget closeButton = Positioned(
+      top: statusBarHeight,
+      right: 0,
+      child: IconButton(
+        padding: const EdgeInsets.all(15),
+        icon: const Icon(
+          Icons.close,
+          color: Colors.white70,
+          size: 26,
+        ),
+        onPressed: () {
+          // show exit dialog
+          exitDialog(context: context);
+        },
+      ),
+    );
+
     return WillPopScope(
       onWillPop: () {
-        // show exit dialog
-        exitDialog(context: context);
+        if (isMobile()) {
+          // show exit dialog
+          exitDialog(context: context);
 
-        // do not close the app when use click back button on Android
-        return Future.value(false);
+          // do not close the app when use click back button on Android
+          return Future.value(false);
+        } else {
+          return Future.value(true);
+        }
       },
       child: Scaffold(
         body: Stack(
           children: [
-            GestureDetector(
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                color: backgroundColor,
-                child: Center(
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 500),
-                    curve: Curves.easeInOut,
-                    transform: Matrix4.identity()..scale(scaleFactor),
-                    transformAlignment: Alignment.center,
-                    child: Text(
-                      'Hello there!',
-                      style: GoogleFonts.aBeeZee(
-                        // adopt font size to screen width
-                        fontSize: 25 * screenWidth/200,
-                        fontWeight: FontWeight.bold,
-                        color: previousColor,
-                        shadows: [
-                          Shadow(
-                            blurRadius: 12,
-                            color: previousColor,
-                            offset: isMobile()
-                              ? const Offset(3, 3)
-                              : const Offset(10, 10),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              onTap: () {
-                // start text scale up
-                animateText();
-                // get random color and update background color with it
-                setState(() {
-                  previousColor = backgroundColor;
-                  backgroundColor = getRandomColor();
-                });
-              },
-            ),
-            Positioned(
-              top: statusBarHeight,
-              right: 0,
-              child: IconButton(
-                padding: const EdgeInsets.all(15),
-                icon: const Icon(
-                  Icons.close,
-                  color: Colors.white70,
-                  size: 26,
-                ),
-                onPressed: () {
-                  // show exit dialog
-                  exitDialog(context: context);
-                },
-              ),
-            ),
+            animatedContainer,
+            if (isMobile()) closeButton else Container(),
           ],
         ),
       ),
