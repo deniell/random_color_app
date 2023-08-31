515b3b2a874f39bf2a536a968db0023f28c4955d
Denis Stahovsky <denis.stahovsky@vantino.ch>
2023-08-31 14:11:55 +0300

    - Colored page UI improved to be more interesting (attractive?).

--- lib/pages/colored_page.dart
+++ lib/pages/colored_page.dart
@@ -14,7 +14,11 @@ class ColoredPage extends StatefulWidget {
 
 class _ColoredPageState extends State<ColoredPage> {
   // page background color
-  Color? backgroundColor = Colors.grey[300];
+  Color backgroundColor = Colors.grey[300] ?? Colors.grey;
+  // previous page background color
+  Color previousColor = Colors.grey;
+  // scale factor for text animation
+  double scaleFactor = 1.0;
 
   @override
   Widget build(BuildContext context) {
@@ -23,27 +27,68 @@ class _ColoredPageState extends State<ColoredPage> {
 
     return Scaffold(
       body: GestureDetector(
-        child: Container(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
           color: backgroundColor,
           child: Center(
-            child: Text(
-              'Hello there',
-              style: GoogleFonts.aBeeZee(
-                // adopt font size to screen width
-                fontSize: 25 * screenWidth/200,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut,
+              transform: Matrix4.identity()..scale(scaleFactor),
+              transformAlignment: Alignment.center,
+              child: Text(
+                'Hello there!',
+                style: GoogleFonts.aBeeZee(
+                  // adopt font size to screen width
+                  fontSize: 25 * screenWidth/200,
+                  fontWeight: FontWeight.bold,
+                  color: previousColor,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 12,
+                      color: previousColor,
+                      offset: const Offset(10, 10),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         ),
         onTap: () {
+          // start text scale up
+          animateText();
           // get random color and update background color with it
           setState(() {
+            previousColor = backgroundColor;
             backgroundColor = getRandomColor();
           });
         },
       ),
     );
+  }
+
+  ///
+  /// Function responsible for text animation
+  /// The animation scale up the text and after [delay] scale it down to
+  /// default scale factor
+  ///
+  void animateText() {
+    // value which define max text scale up factor
+    const scaleUpValue = 2.2;
+    // delay in milliseconds, which define when scale up animation will be
+    // interrupted and scale down animation will be activated
+    const delay = 100;
+
+    // scale up text
+    setState(() {
+      scaleFactor = scaleUpValue;
+    });
+    // return text to initial size
+    Future.delayed(const Duration(milliseconds: delay), () {
+      setState(() {
+        scaleFactor = 1.0;
+      });
+    });
   }
 }
