2210927db7485e5852c3f34a75a102b25d9b302f
Denis Stahovsky <denis.stahovsky@vantino.ch>
2023-08-31 11:26:15 +0300

    - get rid of dart:math library in logic for random code generation; - implemented custom algorithm for random color generation.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,13 +1,13 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
 ///
 /// Generate and return random Color
 /// 
-Color? getRandomColor() {
+Color getRandomColor() {
   // the alpha value, with 0 being transparent and 255 being fully opaque.
   const int opacity = 255;
 
+  // construct a color from the lower 8 bits of four integers
   return Color.fromARGB(
     opacity,
     getRandom8BitNumber(),
@@ -20,5 +20,50 @@ Color? getRandomColor() {
 /// Return random 8 bit number (int in diapason from 0 to 255)
 ///
 int getRandom8BitNumber() {
-  return Random().nextInt(256);
+  // list for random 8 bit
+  final List<int> listOfBits = [];
+  // bits to be added to the list
+  const int bitsToAdd = 8;
+
+  // loop for filling listOfBits with 8 random bits
+  for (int counter = 0; counter < bitsToAdd; counter++) {
+    final bit = getRandomBit();
+    listOfBits.add(bit);
+  }
+
+  // convert bits to integer value
+  final int intValue = bitsToInt(listOfBits);
+  
+  return intValue;
+}
+
+///
+/// Return random bit
+/// This function contain main randomness logic. Current realization do not use
+/// any external (even dart:math) library, but can be sufficiently improved with
+/// use of them.
+///
+int getRandomBit() {
+  // try to get random value from DateTime
+  final now = DateTime.now().microsecondsSinceEpoch;
+  // get hash from current DateTime, to have even more random value
+  final hash = now.hashCode;
+  // convert hash value to bit value (int 0 or 1)
+  final int bit = hash % 2;
+
+  return bit;
+}
+
+///
+/// Convert list of bits to [bits].length integer value
+///
+int bitsToInt(List<int> bits) {
+  // initial result
+  int result = 0;
+  // set given bits to result value
+  for (final int bit in bits) {
+    result = (result << 1) | bit;
+  }
+
+  return result;
 }
